5648ca205f54bb66a575874f465172c17091b853
hapybeing <gaurangk.inbox@gmail.com>
2026-03-15 11:25:17 +0530

    Update useStore.ts

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -3,13 +3,17 @@ import { create } from 'zustand'
 interface UniverseState {
   activeNode: string | null
   viewState: 'macro' | 'transition' | 'micro'
+  scrollProgress: number
   setActiveNode: (id: string | null) => void
   setViewState: (state: 'macro' | 'transition' | 'micro') => void
+  setScrollProgress: (progress: number) => void
 }
 
 export const useStore = create<UniverseState>((set) => ({
   activeNode: null,
   viewState: 'macro',
+  scrollProgress: 0,
   setActiveNode: (id) => set({ activeNode: id }),
   setViewState: (state) => set({ viewState: state }),
+  setScrollProgress: (progress) => set({ scrollProgress: progress }),
 }))
