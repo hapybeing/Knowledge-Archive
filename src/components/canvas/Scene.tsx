b4c89350fc4d8777147ee423ab5efbdf90b27a2e
hapybeing <gaurangk.inbox@gmail.com>
2026-03-15 11:16:12 +0530

    Update Scene.tsx

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -3,23 +3,23 @@
 import { Canvas } from "@react-three/fiber";
 import { Preload } from "@react-three/drei";
 import InstancedStarfield from "./environment/InstancedStarfield";
+import ConstellationGraph from "./nodes/ConstellationGraph";
+import ActiveWorldContainer from "./worlds/ActiveWorldContainer";
 
 export default function Scene() {
   return (
     <Canvas
       camera={{ position: [0, 0, 15], fov: 45, near: 0.1, far: 1000 }}
-      dpr={[1, 2]} // Cap pixel ratio for performance
-      gl={{ antialias: false, powerPreference: "high-performance" }} // Optimized rendering
+      dpr={[1, 2]}
+      gl={{ antialias: false, powerPreference: "high-performance" }}
     >
       <color attach="background" args={["#030305"]} />
-      
-      {/* Ambient environment lighting */}
       <ambientLight intensity={0.2} />
       
-      {/* The background stars */}
       <InstancedStarfield count={15000} />
+      <ConstellationGraph />
+      <ActiveWorldContainer />
 
-      {/* Preloads all assets to prevent stuttering later */}
       <Preload all />
     </Canvas>
   );
